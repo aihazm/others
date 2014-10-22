5e3367550226c876ddcd6206b4dfcbf9f902911d
Aiham Azmeh <aihazm@yahoo.com>
2014-10-22 17:15:41 +0200

    backup purpose

--- MashupScriptParser/app/controllers/mainCtrl.js
+++ MashupScriptParser/app/controllers/mainCtrl.js
@@ -1,8 +1,60 @@
 define(['utils/mashup-generator'],function(mashupGenerator){
 	return function ($scope) {
+		var $the_code = $('#the_code');
 		$scope.run = function(){
-			var $the_code = $('#the_code');			 
-        	var openAppStrs = mashupGenerator.findApps($the_code[0].innerHTML);
-		};        
+			
+		};
+		$scope.findApps = function(){			
+        	var apps = mashupGenerator.findApps($the_code[0].innerHTML);
+        	document.getElementById('the_res').innerHTML='var apps = '+JSON.stringify(apps,null,'\t')+';';
+        	$('pre code').each(function(i, block) { hljs.highlightBlock(block);});
+		};
+		$scope.findAppById = function($event){			
+			var appId = $($event.currentTarget).parent().find('input').val();			
+			var app = mashupGenerator.findAppById(appId,$the_code[0].innerHTML);
+			document.getElementById('the_res').innerHTML='/* for app with ID '+appId+'*/\nvar app = '+JSON.stringify(app,null,'\t')+';';
+			$('pre code').each(function(i, block) { hljs.highlightBlock(block);});
+		};
+		$scope.findAppByVar = function($event){			
+			var appVar = $($event.currentTarget).parent().find('input').val();			
+			var app = mashupGenerator.findAppByVar(appVar,$the_code[0].innerHTML);
+			document.getElementById('the_res').innerHTML='/* for app with var '+appVar+'*/\nvar app = '+JSON.stringify(app,null,'\t')+';';
+			$('pre code').each(function(i, block) { hljs.highlightBlock(block);});
+		};
+		$scope.findAppMethods = function($event){			
+			var appVar = $($event.currentTarget).parent().find('input').val();			
+			var methods = mashupGenerator.findAppMethods(appVar,$the_code[0].innerHTML);
+			document.getElementById('the_res').innerHTML='/* for app with var '+appVar+'*/\nvar methods = '+JSON.stringify(methods,null,'\t')+';';
+			$('pre code').each(function(i, block) { hljs.highlightBlock(block);});
+		};
+		$scope.addVisualization = function($event){
+			var appVar = $($event.currentTarget).parent().find('input.app').val();
+			var params = ['elementId','objectId'];
+			var result_string = mashupGenerator.addVisualization($the_code[0].innerHTML, appVar, params);
+			document.getElementById('the_res').innerHTML=result_string;
+			document.getElementById('the_code').innerHTML=result_string;
+			$('pre code').each(function(i, block) { hljs.highlightBlock(block);});
+		};
+		$scope.delVisualization = function($event){
+			var visId = $($event.currentTarget).parent().find('input.vis').val();
+			var result_string = mashupGenerator.delVisualization($the_code[0].innerHTML,visId);
+			document.getElementById('the_res').innerHTML=result_string;
+			document.getElementById('the_code').innerHTML=result_string;
+			$('pre code').each(function(i, block) { hljs.highlightBlock(block);});
+		};
+		$scope.getVisualization = function($event){
+			var visId = $($event.currentTarget).parent().find('input.vis').val();
+			var visualization = mashupGenerator.getVisualization($the_code[0].innerHTML,visId);
+			document.getElementById('the_res').innerHTML='var visualization = '+JSON.stringify(visualization,null,'\t')+';';
+			$('pre code').each(function(i, block) { hljs.highlightBlock(block);});
+		};
+		
+		$scope.addCube = function($event){
+			var appVar = $($event.currentTarget).parent().find('input.app').val();
+			var result_string = mashupGenerator.addCube($the_code[0].innerHTML,appVar,['','myFunction']);
+			document.getElementById('the_res').innerHTML=result_string;
+			document.getElementById('the_code').innerHTML=result_string;
+			$('pre code').each(function(i, block) { hljs.highlightBlock(block);});
+		};
     };
 });
--- MashupScriptParser/app/utils/mashup-generator.js
+++ MashupScriptParser/app/utils/mashup-generator.js
@@ -1,8 +1,17 @@
 define([],function(){
-	"use strict";
+	"use strict";	
 	/******************************************
 	 * Private functions 
-	 ******************************************/	
+	 ******************************************/
+	
+	/**
+	* removing all comments from the given string (JS comments)
+	* @param {string} the code js string
+	* @returns {string} the code cleaned string
+	*/
+	var removeComentedCode = function(txt){
+		return txt.replace(/(?:\/\*(?:[\s\S]*?)\*\/)|(?:([\s;])+\/\/(?:.*)$)/gm,'');
+	};	
 	/**
 	* Matching all var {xxx} = qlik.openApp... strings
 	* @param {string} the code js string
@@ -13,8 +22,73 @@ define([],function(){
 		var res = txt.match(regExp);
 		if(res){ return _.map(res, function(str){ return str.trim(); });}else{ return[];}
 	};
-	var removeComentedCode = function(txt){		
-		return txt.replace(/(?:\/\*(?:[\s\S]*?)\*\/)|(?:([\s;])+\/\/(?:.*)$)/gm,'');
+	/**
+	* Matching all .{xxx}( strings for a given app variable
+	* @param {string} the code js string
+	* @returns {array} methods strings
+	*/
+	var getAppMethodsStrings = function(appVar,string){
+		string = removeComentedCode(string);
+		if(appVar){
+			var regExp = new RegExp('('+appVar+'\\.(?!openApp)[\\S\\s]+?((\\)\\;)|(\\)\\.)))','g');
+			var res = string.match(regExp);
+			if(res){
+				for(var i=0 ; i<res.length; i++){
+					res[i]=res[i].trim();
+				}
+				return res;
+			}else{
+				return [];
+			}
+		}else{
+			return [];
+		}
+	};
+	/**
+	* Matching method string for extracting method name and given parameters
+	* @param {string} the method string
+	* @returns {array} methods with parameters
+	*/
+	var getMethod = function(methodString){
+		var method = {};
+		var regExp = /\.([^\(]*)/i; //getting method name
+		method['name'] = methodString.match(regExp)[0];
+		var regExp = /\(([\s\S]+?)((\)\;)|(\)\.))/g;
+		var paramsString = methodString.match(regExp)[0];			
+		var params=[];
+		if(paramsString){
+			paramsString= paramsString.substring(1, paramsString.length-2);// removing parantheses
+			var regExpParams = /(\{[\S\s]*\})/g;
+			var obj = paramsString.match(regExpParams);
+			if(obj){// we may have an object
+				try{
+					params.push(obj[0]);
+					paramsString = paramsString.replace(obj,"");
+					var otherParams = paramsString.split(',');
+					for(var p=0; p<otherParams.length; p++){
+						if(otherParams[p].trim()!==''){
+							otherParams[p]=otherParams[p].replace(/("|')/g,'');
+							params.push(otherParams[p].trim());
+						}
+					} 
+				}catch(e){console.log(e);}
+			}else{
+				params=paramsString.split(',');
+				//remove quotes from simple strings
+				for(var k=0; k<params.length; k++){
+					params[k]=params[k].replace(/("|')/g,'').trim();
+				}
+			};
+			method['params'] = params;
+		}
+		return method;
+	};
+	var findVarValue = function(strVar, string){
+		/*string = removeComentedCode(string);
+		var pattern = '/'+strVar+'\\s*=\\s*(\'|")(.*)(\'|")/';
+		console.log(strVar, string, pattern);
+		var regExp = new RegExp(pattern,'g');
+		console.log(string.match(regExp));*/
 	};
 	var findAppId = function(string){
 		var appId = getParamsInStringFn(string)[0];
@@ -25,35 +99,132 @@ define([],function(){
 		}else{
 			return false;
 		}
-	};		
-	var findAppVar = function(string){
-		var regExp = /var\s+([0-9a-zA-Z_$]*)/g;			
-		return string.match(regExp)[1];
 	};
-	var mashup = [];
+	var findAppVar = function(string){
+		var regExp = /var\s+([0-9a-zA-Z_$]+)/;			
+		return string.match(regExp)[1];
+	};	
 	
 	var getMashup = function(){};
 	var parseMashup = function(){};
 	
 	var addApp=function(){};
-	var findApp = function(string){
-		
+	var findAppById = function(appId, string){
+		var apps = findApps(string);
+		var res = false;
+		apps.forEach(function(app){			
+			if(app['appId']===appId){				
+				res = app;
+			}
+		});
+		return res;
+	};
+	var findAppByVar = function(appVar,string){
+		var apps = findApps(string);
+		var res= false;
+		apps.forEach(function(app){
+			if(app['appVar']===appVar){
+				res= app;
+			}
+		});
+		return res;
 	};
 	var findApps = function(string){
+		var apps = [];
 		var strings = getOpenAppStrings(string);
 		strings.forEach(function(str){
-			var appId = findAppId(str);
+			var appId = findAppId(str);			
 			var appVar = findAppVar(str);
-			console.log(appId,appVar);
+			if(appVar){
+				var methods = findAppMethods(appVar, string);
+			}
+			apps.push({
+				appId : appId,
+				appVar : appVar,
+				methods : methods
+			});			
 		});
+		return apps;
 	};
-	var findAppMethods = function(){}
+	var findAppMethods = function(appVar, string){
+		var methods = [];
+		var strings = getAppMethodsStrings(appVar, string);
+		strings.forEach(function(str){
+			var method = getMethod(str);
+			methods.push(method);
+		});
+		return methods;
+	};
+	/* ToDo : get method promise */
+	var addVisualization = function(string, appVar, params){
+		if(appVar.length > 0){
+			params = params.map(function(param){
+				return '\''+param+'\'';
+			});
+			return string+'\n'+appVar+'.getObject('+params+');';
+		}else{
+			return string;
+		}
+	};
+	var delVisualization = function(string,visId){
+		var regExp = '([A-Za-z0-9$_]*\.getObject\(.*("|\')'+visId+'(\'|")\)(\.|;))';
+		var regExp = new RegExp(regExp,'g');
+		return string.replace(regExp,'');
+	};
+	var getVisualization = function(string, visId){
+		var vis={};
+		string = removeComentedCode(string);
+		var regExp = '([A-Za-z0-9$_]*\.(getObject|getSnapshot)\(.*("|\')'+visId+'(\'|")\)(\.|;))';
+		var regExp = new RegExp(regExp,'g');
+		var res = string.match(regExp);
+		if(res){
+			var regExp = /\.([^\(]*)/i; //getting method name
+			vis['methodName'] = res[0].match(regExp)[0];
+			regExp = /\(([^)].*)\)/g;
+			var betweenParenthese = res[0].match(regExp)[0];
+			betweenParenthese = betweenParenthese.substring(1, betweenParenthese.length-1);			
+		}
+		if(betweenParenthese && betweenParenthese.length > 0 ){
+			var res= betweenParenthese.split(',');			
+			for(var i=0; i<res.length; i++){
+				res[i] = res[i].replace(/("|')/g,'').trim();
+			}
+			vis['id']=res[1];
+			vis['elementId']=res[0];
+		}
+		return vis;
+	};
 	
-	var addVisualization = function(){}
-	var delVisualization = function(){}
-	var getVisualization = function(){}
-	
-	var addCube = function(){};
+	var addCube = function(string, appVar, params){
+		if(appVar.length > 0){
+			var cube = {
+				qDimensions : [{
+					qDef : {
+						qFieldDefs : ["FirstName"]
+					}
+				}, {
+					qDef : {
+						qFieldDefs : ["LastName"]
+					}
+				}],
+				qMeasures : [{
+					qDef : {
+						qDef : "1"
+					}
+				}],
+				qInitialDataFetch : [{
+					qTop : 0,
+					qLeft : 0,
+					qHeight : 20,
+					qWidth : 3
+				}]
+			};
+			
+			return string+'\n'+appVar+'.createCube('+JSON.stringify(cube,null,'\t')+','+params[1]+');';
+		}else{
+			return string;
+		}
+	};
 	var updateCube = function(){};
 	var addList = function(){};
 	var updateList = function(){};
@@ -62,6 +233,17 @@ define([],function(){
 	
 	
 	return {
-		findApps : findApps
+		/* Apps */
+		findApps : findApps,
+		findAppById : findAppById,
+		findAppByVar : findAppByVar, 
+		findAppMethods : findAppMethods,
+		/* Visualizations */
+		addVisualization: addVisualization,
+		delVisualization: delVisualization,
+		getVisualization: getVisualization,
+		/* Cubes and Lists */
+		addCube : addCube,
+		addList : addList
 	};
 });
